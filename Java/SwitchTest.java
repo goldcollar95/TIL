96218001d3d69ca6ad78595729bb5c186dbdc877
Sangmun Kim <98082293+goldcollar95@users.noreply.github.com>
2022-06-21 23:57:43 +0900

    Create SwitchTest.java

---
+++ Java/SwitchTest.java
@@ -0,0 +1,51 @@
+package chap3;
+
+public class SwitchTest {
+
+	public static void main(String[] args) {
+	/*	//1-6월 중에서 각 월의 몇일까지 있는지 출력
+		// 1월 - 31일까지 있습니다.
+		int month =(int)(Math.random() *6) +1;//1에서 6까지 난수발생 1-6
+		int day = 0;
+		
+		switch(month) {
+		case 1: //1월일때
+			 day = 31;
+			 break;
+		case 2:
+			 day = 28;
+			 break;
+		case 3:
+			 day = 31;
+			 break;
+		case 4:
+			 day = 30;
+			 break;
+		case 5:
+			 day = 31;
+			 break;
+		default:
+			 day = 30;			
+		}//switch
+
+		System.out.println(month + " 월은 " + day + " 일만큼 있습니다."); */
+	/*	int avg = (int)(Math.random() *100) + 0;
+		char alpabet = 'A';
+		switch(avg) {
+		case 90:          //case뒤에는 연산자가 올수없고 숫자만 올수있다.
+			alpabet = 'A';
+			break;
+		case 70:
+			alpabet = 'B';
+			break;
+		case 50:
+			alpabet = 'C';
+			break;
+		default:
+			alpabet = 'F';
+		
+		}
+		System.out.println(avg + "평균은" + alpabet + "다음과 같습니다."); */
+		
+	}
+}
