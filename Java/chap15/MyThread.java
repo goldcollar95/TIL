1bfc3afa513ed0938f9658a6e13029e67d3a6191
Sangmun Kim <98082293+goldcollar95@users.noreply.github.com>
2022-07-07 09:40:11 +0900

    Update MyThread.java

--- Java/chap15/MyThread.java
+++ Java/chap15/MyThread.java
@@ -77,3 +77,83 @@ public class MyThreadTest extends Thread {
 	}
 
 }
+
+-------------------------------------------
+*답안*
+
+package chap15;
+class MusicThread extends Thread{
+	//상속 . 생성자. 오버라이딩 - 음악듣는중입니다 3번 출력
+	//오버라이딩 run
+	MusicThread(String name){
+		super(name);
+	}
+	
+ 
+	public void run() {
+		for(int i = 1; i <= 3; i++) {
+			System.out.println(getName() + "음악듣는 중입니다 =" + i);
+		}
+	}
+}
+
+class DownloadThread extends Thread{
+	//상속 . 생성자. 오버라이딩  - 다운로드중입니다 10번 출력
+	
+	public DownloadThread(String name){
+		super(name);
+	}
+	public void run() {
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(getName() + "다운로드 중입니다 =" + i);
+		}
+	}
+}
+
+class NewsThread extends Thread{
+	//상속 . 생성자. 오버라이딩  - 뉴스보는 중입니다 5번 출력	
+	String news;
+	
+	NewsThread(String name){
+		super(name);
+	}
+	public void run() {
+		for(int i=0; i<=5; i++) {
+			System.out.println(getName() + "뉴스보는 중입니다 =" +i );
+			
+		}
+	}
+}
+//arr값에 인덱스가 도착할때, 하나씩 선언하여 쓸수있게끔 선언해야함
+public class MyThreadTest2 extends Thread {
+
+	public static void main(String[] args) {
+		Thread arr[] = new Thread[3]; //class A extends Thread 
+		arr[0] = new MusicThread("음악");
+		
+		arr[1] = new NewsThread("뉴스");
+		
+		arr[2] = new DownloadThread("다운로드");
+		
+		for(int i = 0; i < arr.length; i++) {
+		arr[i].start();
+		}
+		
+		/*3개 스레드 실행 시작 메소드 호출 */
+		
+		/* 음악듣는중입니다
+		 * 음악듣는중입니다
+		 * 음악듣는중입니다
+		 * 다운로드중입니다
+		 * 뉴스보는 중입니다
+		 * 음악듣는중입니다
+		 * 음악듣는중입니다
+		 * ....
+		 * 
+		 *   */
+		
+		
+
+	}
+
+}
