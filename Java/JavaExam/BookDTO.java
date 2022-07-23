22e90271ebf6ffd4eac993507580ad12e6f9fe90
Sangmun Kim <98082293+goldcollar95@users.noreply.github.com>
2022-07-23 23:39:09 +0900

    Create BookDTO.java

---
+++ Java/JavaExam/BookDTO.java
@@ -0,0 +1,48 @@
+package javaExam;
+
+public class BookDTO {
+	String bookNo;
+	String bookTitle;
+	String bookAuthor;
+	String bookYear;
+	int bookPrice;
+	String bookPublisher;
+	public String getBookNo() {
+		return bookNo;
+	}
+	public void setBookNo(String bookNo) {
+		this.bookNo = bookNo;
+	}
+	public String getBookTitle() {
+		return bookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+	public String getBookYear() {
+		return bookYear;
+	}
+	public void setBookYear(String bookYear) {
+		this.bookYear = bookYear;
+	}
+	public int getBookPrice() {
+		return bookPrice;
+	}
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	public String getBookPublisher() {
+		return bookPublisher;
+	}
+	public void setBookPublisher(String bookPublisher) {
+		this.bookPublisher = bookPublisher;
+	}
+	
+
+}
