9bd3beb3910402265a58e50816de82d0bfb79578
Sangmun Kim <98082293+goldcollar95@users.noreply.github.com>
2022-07-23 23:38:17 +0900

    Update book.sql

--- mysql/book.sql
+++ mysql/book.sql
@@ -6,4 +6,20 @@ insert into book (bookNo, bookTitle, bookAuthor, bookYear, bookPrice, bookPublis
 insert into book (bookNo, bookTitle, bookAuthor, bookYear, bookPrice, bookPublisher) values('B002', 'MYSQL이해', '나이해', '2022-01-01', '25000', '캠퍼스출판');
 insert into book (bookNo, bookTitle, bookAuthor, bookYear, bookPrice, bookPublisher) values('B003', 'SPRING 활용', '김고수', '2021-01-01', '45000', '디지털출판사');
 
-update book set bookyear = '2020-01-01' where bookyear = '2020';
+
+select * , to_char('2020-01-01', 'yyyy') from book;
+
+select *, date_format(bookYear,'%Y')as bookYearonly from book ;
+
+
+select *, date_format('2022-01-01','%Y')as bookYearonly from book where bookNo = 'B002';
+select *, date_format('2021-01-01','%Y')as bookYearonly from book where bookNo = 'B003';
+
+update book set bookyear = '2020' where bookNo = 'B001';
+
+
+
+
+
+
+
